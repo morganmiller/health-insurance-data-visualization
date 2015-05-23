b40543c1edfe756b5d76e929ca14445632b3b501
Josh Cheek <josh.cheek@gmail.com>
2015-05-23 17:56:33 -0600

    Write test names / first failing test
    Fails b/c needs a test DB

--- Rakefile
+++ Rakefile
@@ -2,3 +2,9 @@ desc 'Start the server for the application'
 task 'server' do
   sh 'rackup config.ru'
 end
+
+
+desc 'Run my tests!'
+task 'test' do
+  sh 'mrspec -I .'
+end
---
+++ test/app_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/spec'
+require 'rack/test'
+require 'mah_app'
+
+class AppTest < Minitest::Spec
+  include Rack::Test::Methods
+
+  def app
+    MahApp
+  end
+
+  it 'GET to /plans lists health care plan data' do
+    plan = Plan.create state: 'AK', child_premium: 123, plan_type: 'PPO'
+
+    response = get '/plans'
+    assert_equal 200, response.status
+    assert_equal 'application/json', response.content_type
+
+    assert_equal [{id: plan.id, state: 'AK', child_premium: 123, plan_type: 'PPO'}],
+                 JSON.parse(response.body, symbolize_names: true)
+  end
+
+  it 'GET to /plans/id shows data for the plan with that id'
+  it 'if the id doesn\'t exist, renders a 404 with a description of the error'
+end
