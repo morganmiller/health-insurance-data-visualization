7e0a069b41617512c4803ea25c7e4243dc3021ea
Morgan Miller <m.miller722@gmail.com>
2015-05-23 16:49:58 -0600

    begin importing data from json

---
+++ import.rb
@@ -0,0 +1,17 @@
+require 'json'
+require_relative 'plan_manager'
+
+data = JSON.parse File.read("#{__dir__}/data.json")
+
+column_map = data["meta"]["view"]["columns"].reject { |c| c["id"] == -1 }
+  .map { |c| [c["name"], c["id"].to_s] }.to_h
+
+data["data"].each do |column|
+    Plan.create state:         column[column_map["State"]],
+                child_premium: column[column_map["Premium Child"]],
+                plan_type:     column[column_map["Plan Type"]]
+end
+
+Plan.count
+Plan.pluck :child_premium
+Plan.pluck :plan_type
