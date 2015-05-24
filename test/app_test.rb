7df88347ca9eae9be0f42d80d66faa1b1f53b5e9
Morgan Miller <m.miller722@gmail.com>
2015-05-23 19:05:17 -0600

    Refactor tests

--- test/app_test.rb
+++ test/app_test.rb
@@ -1,32 +1,32 @@
 require 'test_helper'
 
 class AppTest < Minitest::Spec
+  attr_reader :plan
+  
   include Rack::Test::Methods
 
   def app
     MahApp
   end
-
-  it 'GET to /plans lists health care plan data' do
-    plan = Plan.create state: 'AK', child_premium: 123, plan_type: 'PPO'
-
-    response = get '/plans'
-    assert_equal 200, response.status
+  
+  def setup
+    @plan = Plan.create state: 'AK', child_premium: 123, plan_type: 'PPO'
+  end
+  
+  def assert_gets(path, json, status: 200)
+    response = get path
+    assert_equal status, response.status
     assert_equal 'application/json', response.content_type
 
-    assert_equal [{id: plan.id, state: 'AK', child_premium: 123, plan_type: 'PPO'}],
-                 JSON.parse(response.body, symbolize_names: true)
+    assert_equal json, JSON.parse(response.body, symbolize_names: true)
+  end
+
+  it 'GET to /plans lists health care plan data' do
+    assert_gets '/plans', [{id: plan.id, state: 'AK', child_premium: 123, plan_type: 'PPO'}]
   end
 
   it 'GET to /plans/id shows data for the plan with that id' do
-    plan = Plan.create state: 'AK', child_premium: 123, plan_type: 'PPO'
-
-    response = get "/plans/#{plan.id}"
-    assert_equal 200, response.status
-    assert_equal 'application/json', response.content_type
-
-    assert_equal({id: plan.id, state: 'AK', child_premium: 123, plan_type: 'PPO'},
-      JSON.parse(response.body, symbolize_names: true))
+    assert_gets "/plans/#{plan.id}", {id: plan.id, state: 'AK', child_premium: 123, plan_type: 'PPO'}
   end
   
   it 'if the id doesn\'t exist, renders a 404 with a description of the error'
