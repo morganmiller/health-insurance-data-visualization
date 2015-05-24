488614366b85922c0b9b66926e19ed45599376e1
Morgan Miller <m.miller722@gmail.com>
2015-05-23 18:50:41 -0600

    Run each test with isolated data

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,12 @@ root_dir = File.expand_path '..', __dir__
 app_env = AppEnv.new(root_dir, 'test')
 app_env.reset
 require 'mah_app'
+
+RSpec.configure do |config|
+  config.around do |spec|
+    ActiveRecord::Base.transaction do
+      spec.call
+      raise ActiveRecord::Rollback
+    end
+  end
+end
